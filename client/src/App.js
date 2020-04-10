c1662b22143d836a91f936fec669e9d1d4728140
clueking1 <wcoan300@gmail.com>
2020-04-10 11:41:05 -0400

    added private route so user must be authnticated before members page shows

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,37 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 import Home from "./pages/home";
 import Signup from "./pages/signup";
 import Member from "./pages/member"
 import './login.css'
 
+const PrivateRoute = ({ component: Component, isAuthenticated, ...rest}) => (
+  <Route
+    {...rest}
+    render={props => (
+      isAuthenticated
+      ? (
+         <Component {...props} />
+      )
+      : (<Redirect to={{ pathname: '/login', state: { from: props.location} }} />)
+    )}
+  />
+  );
+
 function App() {
   return (
     <Router>
       <div>
+        <Switch>
         <Route exact path="/" component={Home} />
         <Route  path="/login" component={Home} />
         <Route  path="/signup" component={Signup} />
-        <Route  path="/member" component={Member} />
+        <PrivateRoute  path="/member" component={Member} />
+        </Switch>
       </div>
     </Router>
   );
   
 }
-
-
 
 export default App;
