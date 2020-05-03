800f30a0c328ab185c567c33f648aded9c8c43da
clueking1 <wcoan300@gmail.com>
2020-05-03 18:43:17 -0400

    took out homepage in package.json server side

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ if (process.env.NODE_ENV === "production") {
 }
 
 app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./client/build"));
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 
